25ed0ec91364ef6447ddb192445bb6ce7a11c815
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-25 05:57:54 +0000

    c

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -12,6 +13,25 @@ class homeScreen extends StatefulWidget {
 class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.pink],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: Text(
+          "Quebec centre",
+          style: GoogleFonts.montserrat(
+              fontSize: 20, letterSpacing: 3, fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(),
+    );
   }
 }
