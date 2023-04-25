feb5b3520ea5a4dfc0eaeb764e7227bffda4dbab
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-25 06:50:57 +0000

    c

---
+++ lib/screens/settingScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class settingsScreen extends StatefulWidget {
+  const settingsScreen({super.key});
+
+  @override
+  State<settingsScreen> createState() => _settingsScreenState();
+}
+
+class _settingsScreenState extends State<settingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Paramétres",
+          style: GoogleFonts.montserrat(letterSpacing: 2),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
