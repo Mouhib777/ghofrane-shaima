7bff7f2d6f8105048b4dbbe7c6ef7f20c938597f
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-27 10:05:21 +0000

    c

--- lib/cours/cour 7.dart
+++ lib/cours/cour 7.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class cour6 extends StatefulWidget {
+class cour7 extends StatefulWidget {
   // const cour4({super.key});
 
   @override
-  State<cour6> createState() => _cour6State();
+  State<cour7> createState() => _cour7State();
 }
 
-class _cour6State extends State<cour6> {
+class _cour7State extends State<cour7> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cour 4 ")),
+      appBar: AppBar(title: Text("Cour 7 ")),
     );
   }
 }
