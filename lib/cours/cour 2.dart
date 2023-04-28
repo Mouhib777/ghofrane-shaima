1213f040624e5dd1362f534253c2c390960755f4
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-28 12:41:19 +0000

    c

--- lib/cours/cour 2.dart
+++ lib/cours/cour 2.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class cour2 extends StatefulWidget {
   // const cour3({super.key});
@@ -10,13 +12,130 @@ class cour2 extends StatefulWidget {
 }
 
 class _cour2State extends State<cour2> {
+  int currentStep = 0;
+  List<Step> getSteps() => [
+        Step(
+          state: currentStep > 0 ? StepState.complete : StepState.indexed,
+          isActive: currentStep >= 0,
+          title: Text(
+            '',
+            style: GoogleFonts.raleway(fontSize: 12),
+          ),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                "assets/images/12.png",
+                height: 155,
+              ),
+              Text(
+                "• Nom* : Dupont\n• Prénom : Éric\n• Sexe : Masculin (un homme) Féminin (une femme)\n• Né(e) le : 22 février 1995**\n• À : New Delhi (Inde)\n• Nationalité : indienne",
+                style: GoogleFonts.montserrat(fontSize: 16),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              )
+            ],
+          ),
+        ),
+      ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("L'état civil et la nationalité :"),
-        centerTitle: true,
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            "L'état civil et la nationalité :",
+            style: GoogleFonts.montserrat(
+                letterSpacing: 1, fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
+        ),
+        body: Stepper(
+            elevation: 4,
+            type: StepperType.vertical,
+            steps: getSteps(),
+            currentStep: currentStep,
+            onStepContinue: () {
+              final lastStep = currentStep == getSteps().length - 1;
+
+              if (lastStep) {
+                print('okk');
+              } else {
+                setState(() {
+                  currentStep += 1;
+                });
+              }
+            },
+            onStepCancel: () {
+              currentStep == 0
+                  ? null
+                  : setState(() {
+                      currentStep -= 1;
+                    });
+            },
+            controlsBuilder: (BuildContext context, ControlsDetails) {
+              final lastStep = currentStep == getSteps().length - 1;
+              return Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 130,
+                      child: Expanded(
+                          child: ElevatedButton(
+                        onPressed: ControlsDetails.onStepContinue,
+                        child: Text(
+                          lastStep ? '' : '',
+                          style: GoogleFonts.raleway(),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            shadowColor: Colors.transparent,
+                            backgroundColor: lastStep
+                                ? Colors.transparent
+                                : Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide.none,
+                                borderRadius: BorderRadius.circular(15))),
+                      )),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    if (currentStep != 0)
+                      SizedBox(
+                        width: 100,
+                        child: Expanded(
+                            child: ElevatedButton(
+                          onPressed: ControlsDetails.onStepCancel,
+                          // lastStep ?  : ControlsDetails.onStepCancel,
+
+                          child: Text(
+                            'Précedent',
+                            style: GoogleFonts.raleway(),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15))),
+                        )),
+                      ),
+                  ],
+                ),
+              );
+            }));
   }
 }
