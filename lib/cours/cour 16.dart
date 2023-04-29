32fba40881a864540615e7b20e275c5e88247e41
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-29 15:10:19 +0000

    c

--- lib/cours/cour 16.dart
+++ lib/cours/cour 16.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class cour16 extends StatefulWidget {
   @override
@@ -12,7 +13,10 @@ class _cour16State extends State<cour16> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cour 16 "),
+        title: Text(
+          "Les transports",
+          style: GoogleFonts.montserrat(),
+        ),
         centerTitle: true,
       ),
       body: Column(
