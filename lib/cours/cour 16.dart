e19767f9340923f003f076df51af29d024f34fe2
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-29 18:20:13 +0000

    c

--- lib/cours/cour 16.dart
+++ lib/cours/cour 16.dart
@@ -96,7 +96,155 @@ class _cour16State extends State<cour16> {
             Text("Cheval"),
             SizedBox(
               height: 30,
-            )
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "Voyager/Partir en :",
+                  style: GoogleFonts.montserrat(),
+                ),
+              ],
+            ),
+            Image.asset(
+              "assets/images/73-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Voiture"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/74-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Taxi"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/75-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Autobus"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/76-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Tramway"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/77-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Métro"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/78-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Train"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/79-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Avion"),
+            SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              "assets/images/80-removebg-preview.png",
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.speaker_3_fill,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              "assets/images/81-removebg-preview.png",
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.speaker_3_fill,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Un ticket de métro/de bus"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/82-removebg-preview.png",
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.speaker_3_fill,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Un aller-retour (2 tickets)"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/83-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Un carnet = 10 tickets"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/84-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Un billet de train"),
+            SizedBox(
+              height: 50,
+            ),
           ],
         ),
       ),
