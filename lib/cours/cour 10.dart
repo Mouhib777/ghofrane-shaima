11027b74d968d9904b926d0d18fb9f7e79e7cb36
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-29 12:36:50 +0000

    c

--- lib/cours/cour 10.dart
+++ lib/cours/cour 10.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class cour10 extends StatefulWidget {
   @override
@@ -9,13 +11,61 @@ class _cour10State extends State<cour10> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cour 10 "),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            "La date",
+            style: GoogleFonts.montserrat(),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset("assets/images/27-removebg-preview.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.speaker_3_fill,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset("assets/images/28-removebg-preview.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.speaker_3_fill,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset("assets/images/29-removebg-preview.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    CupertinoIcons.speaker_3_fill,
+                    size: 40,
+                  ),
+                ),
+              ])),
+        ));
   }
 }
