d5dc1651a4032eb587a80c0ee8f92ff1bd72e207
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-29 15:16:57 +0000

    c

--- lib/cours/cour 16.dart
+++ lib/cours/cour 16.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -19,8 +20,85 @@ class _cour16State extends State<cour16> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Image.asset(
+              "assets/images/68-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.speaker_3_fill,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "Voyager/Partir à :",
+                  style: GoogleFonts.montserrat(),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/69-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Pied"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/70-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Velo"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/71-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Moto"),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/72-removebg-preview.png",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Cheval"),
+            SizedBox(
+              height: 30,
+            )
+          ],
+        ),
       ),
     );
   }
