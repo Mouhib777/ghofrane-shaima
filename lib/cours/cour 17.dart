022357dec29b95e6da393c70a9886e073be41e95
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-30 15:45:02 +0000

    c

--- lib/cours/cour 17.dart
+++ lib/cours/cour 17.dart
@@ -20,43 +20,114 @@ class _cour17State extends State<cour17> {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Image.asset(
-              "assets/images/86-removebg-preview.png",
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                CupertinoIcons.speaker_3_fill,
-                size: 40,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Image.asset(
-              "assets/images/87-removebg-preview.png",
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                CupertinoIcons.speaker_3_fill,
-                size: 40,
+              Image.asset(
+                "assets/images/86-removebg-preview.png",
               ),
-            ),
-          ],
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/87-removebg-preview.png",
+              ),
+              Text("Jules est devant la voiture"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/88-removebg-preview.png",
+              ),
+              Text("Lucie est derrière la porte"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/89-removebg-preview.png",
+              ),
+              Text("Paul est à gauche de la maison"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/90-removebg-preview.png",
+              ),
+              Text("Paul est à droite de la maison"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                "assets/images/91-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
