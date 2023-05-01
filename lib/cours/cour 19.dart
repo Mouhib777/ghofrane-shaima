a4287f94060d23bb155aa49c47a21e6c203c5552
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-01 10:30:40 +0000

    c

--- lib/cours/cour 19.dart
+++ lib/cours/cour 19.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class cour19 extends StatefulWidget {
   // const cour2({super.key});
@@ -13,7 +14,21 @@ class _cour19State extends State<cour19> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("À la boulangerie")),
+      appBar: AppBar(
+        title: Text(
+          "À la boulangerie",
+          style: GoogleFonts.montserrat(letterSpacing: 2),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Image.asset("assets/images/110-removebg-preview.png")],
+          ),
+        ),
+      ),
     );
   }
 }
