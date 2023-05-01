e2c64a89f2965a3a6148b5432c874f8ff412344a
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-01 10:27:39 +0000

    c

--- lib/cours/cour 18.dart
+++ lib/cours/cour 18.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -19,8 +20,276 @@ class _cour18State extends State<cour18> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/94-removebg-preview.png",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Une rue'),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/95-removebg-preview.png",
+              ),
+              Text("Une place"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/96-removebg-preview.png",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Une avenue"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/97-removebg-preview.png",
+              ),
+              Text("Paul est à gauche de la maison"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Image.asset(
+                "assets/images/98-removebg-preview.png",
+              ),
+              Text("Paul est à droite de la maison"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                "assets/images/99-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/100-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/101.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/102-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/103-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/104-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/105-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/106-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/107-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/108-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/109-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+              Image.asset(
+                "assets/images/110-removebg-preview.png",
+              ),
+              Text("Le chat est sous la table"),
+              SizedBox(
+                height: 40,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.speaker_3_fill,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
