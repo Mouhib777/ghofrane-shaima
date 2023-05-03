a138f0ffcc91befeb53dce65f5841ef114aef53d
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-03 17:01:36 +0000

    c

--- lib/admin/cour 40.dart
+++ lib/admin/cour 40.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class admin1 extends StatefulWidget {
+  const admin1({super.key});
+
+  @override
+  State<admin1> createState() => _admin1State();
+}
+
+class _admin1State extends State<admin1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
