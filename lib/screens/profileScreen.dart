01940c9c870ff9553df5546295a88dd29196d31b
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-07 20:30:02 +0000

    c

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -1,4 +1,6 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -12,6 +14,27 @@ class profileScreen extends StatefulWidget {
 }
 
 class _profileScreenState extends State<profileScreen> {
+  @override
+  void initState() {
+    getUser_Data();
+    super.initState();
+  }
+
+  var user_data;
+
+  Future<DocumentSnapshot> getUser_Data() async {
+    final User? user1 = FirebaseAuth.instance.currentUser;
+    String? _uid = user1!.uid;
+    var result1 = await FirebaseFirestore.instance
+        .collection('utilisateur')
+        .doc(_uid)
+        .get();
+    setState(() {
+      user_data = result1;
+    });
+    return result1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,9 +56,81 @@ class _profileScreenState extends State<profileScreen> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  user_data["nom"],
+                  style: GoogleFonts.montserratAlternates(
+                      fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "changer votre nom",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  user_data["prenom"],
+                  style: GoogleFonts.montserratAlternates(
+                      fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "changer votre prenom",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  user_data["password"],
+                  style: GoogleFonts.montserratAlternates(
+                      fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "changer votre mot de passe",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
