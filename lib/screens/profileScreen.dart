2d7d7b80a5bfa6883a363dbc8f018a1f150835ed
Mouhib777 <mouhibsahbani@gmail.com>
2023-05-07 20:42:35 +0000

    c

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class profileScreen extends StatefulWidget {
@@ -72,7 +73,67 @@ class _profileScreenState extends State<profileScreen> {
                   width: 20,
                 ),
                 InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        String? text;
+                        return AlertDialog(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'modifier votre nom',
+                                style: GoogleFonts.montserratAlternates(),
+                              ),
+                            ],
+                          ),
+                          content: TextField(
+                            onChanged: (value) {
+                              text = value;
+                            },
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'Annuler',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'mettre à jour',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () async {
+                                final User? user =
+                                    FirebaseAuth.instance.currentUser;
+                                final _uid = user!.uid;
+                                await FirebaseFirestore.instance
+                                    .collection('utilisateur')
+                                    .doc(_uid)
+                                    .update({"nom": text});
+                                EasyLoading.showSuccess(
+                                    "Votre nom à été mettre a jour");
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
                   child: Text(
                     "changer votre nom",
                     style: TextStyle(color: Colors.blue),
@@ -95,7 +156,67 @@ class _profileScreenState extends State<profileScreen> {
                   width: 20,
                 ),
                 InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        String? text;
+                        return AlertDialog(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'modifier votre prenom',
+                                style: GoogleFonts.montserratAlternates(),
+                              ),
+                            ],
+                          ),
+                          content: TextField(
+                            onChanged: (value) {
+                              text = value;
+                            },
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'Annuler',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'mettre à jour',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () async {
+                                final User? user =
+                                    FirebaseAuth.instance.currentUser;
+                                final _uid = user!.uid;
+                                await FirebaseFirestore.instance
+                                    .collection('utilisateur')
+                                    .doc(_uid)
+                                    .update({"prenom": text});
+                                EasyLoading.showSuccess(
+                                    "Votre prenom à été mettre a jour");
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
                   child: Text(
                     "changer votre prenom",
                     style: TextStyle(color: Colors.blue),
@@ -118,7 +239,90 @@ class _profileScreenState extends State<profileScreen> {
                   width: 20,
                 ),
                 InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        String? text;
+                        return AlertDialog(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'modifier votre mot de passe',
+                                style: GoogleFonts.montserratAlternates(
+                                    fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          content: TextField(
+                            onChanged: (value) {
+                              text = value;
+                            },
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'Annuler',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle:
+                                    Theme.of(context).textTheme.labelLarge,
+                              ),
+                              child: Text(
+                                'mettre à jour',
+                                style: GoogleFonts.montserrat(),
+                              ),
+                              onPressed: () async {
+                                final User? user =
+                                    FirebaseAuth.instance.currentUser;
+                                final _uid = user!.uid;
+                                await FirebaseFirestore.instance
+                                    .collection('utilisateur')
+                                    .doc(_uid)
+                                    .update({"password": text});
+                                EasyLoading.showSuccess(
+                                    "Votre password à été mettre a jour");
+                                try {
+                                  // First, sign in with the user's email and current password
+                                  UserCredential userCredential =
+                                      await FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                    email: user_data["email"],
+                                    password: user_data["password"],
+                                  );
+
+                                  // Then, update the user's password with the new password
+                                  await userCredential.user!
+                                      .updatePassword(text!);
+
+                                  print("Password updated successfully");
+                                } on FirebaseAuthException catch (e) {
+                                  if (e.code == 'wrong-password') {
+                                    print('Wrong password provided');
+                                  } else {
+                                    print(
+                                        'Error updating password: ${e.message}');
+                                  }
+                                }
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
                   child: Text(
                     "changer votre mot de passe",
                     style: TextStyle(color: Colors.blue),
